7d92f99f79a6801254fdba6f1b8e05d825b3bc15
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-06-05 09:14:09 +0100

    Create icons for home page

--- src/components/Socials/SocialsItem.js
+++ src/components/Socials/SocialsItem.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import { IconContext } from "react-icons";
+
 const SocialsItem = props => (
   <li className="socialsList__item">
     <a className="socialsList__link" href={props.link} target="_blank" rel="noopener noreferrer">
-      {props.children}
+      <IconContext.Provider value={{color: props.color, className: "social-icon"}}>
+          {props.children}
+      </IconContext.Provider>
     </a>
   </li>
 )
--- src/components/Socials/SocialsList.js
+++ src/components/Socials/SocialsList.js
@@ -1,47 +1,54 @@
 import React from 'react';
 
 import SocialsItem from './SocialsItem';
-import { IconContext } from "react-icons";
 import { FaFacebookSquare } from 'react-icons/fa';
 import { FaGithubSquare } from 'react-icons/fa';
 import { FaLinkedin } from 'react-icons/fa';
 import { FaInstagramSquare } from 'react-icons/fa';
 
-const Socials = () => (
+const SocialsList = () => (
   <ul className="socialsList">
-    <SocialsItem link="#" >
-      <IconContext.Provider value={{color: '#3b5998'}}>
-        <div>
-          <FaFacebookSquare />
-        </div>
-      </IconContext.Provider>
+    <SocialsItem link="#" color="#3B5998">
+      <FaFacebookSquare />
     </SocialsItem>
 
-    <SocialsItem link="#" >
-      <IconContext.Provider value={{color: '#1877f2'}}>
-        <div>
+    <SocialsItem link="#" color="#171515">
+      <FaGithubSquare />
+    </SocialsItem>
+
+    <SocialsItem link="#" color="#0976B4">
+      <FaLinkedin />
+    </SocialsItem>
+
+    <SocialsItem link="#" color="#125688">
+      <FaInstagramSquare />
+    </SocialsItem>
+
+    {/* <SocialsItem link="#" >
+      <IconContext.Provider value={{color: "#171515", className: "social-icon"}}>
+        <div className="social-icon__container">
           <FaGithubSquare />
         </div>
       </IconContext.Provider>
     </SocialsItem>
 
     <SocialsItem link="#" >
-      <IconContext.Provider value={{color: '#1877f2'}}>
-        <div>
+      <IconContext.Provider value={{color: "#0976B4", className: "social-icon"}}>
+        <div className="social-icon__container">
           <FaLinkedin />
         </div>
       </IconContext.Provider>
     </SocialsItem>
 
     <SocialsItem link="#" >
-      <IconContext.Provider value={{color: '#1877f2'}}>
-        <div>
+      <IconContext.Provider value={{color: "#125688", className: "social-icon"}}>
+        <div className="social-icon__container">
           <FaInstagramSquare />
         </div>
       </IconContext.Provider>
-    </SocialsItem>
+    </SocialsItem> */}
 
   </ul>
 )
 
-export default Socials;
+export default SocialsList;
