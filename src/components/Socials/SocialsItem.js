5acd36a7d779abd0c3bc247394c3694989d1feec
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-06-05 09:45:50 +0100

    Create icons for home page

--- src/components/Socials/SocialsItem.js
+++ src/components/Socials/SocialsItem.js
@@ -6,7 +6,9 @@ const SocialsItem = props => (
   <li className="socialsList__item">
     <a className="socialsList__link" href={props.link} target="_blank" rel="noopener noreferrer">
       <IconContext.Provider value={{color: props.color, className: "social-icon"}}>
+        <div className="social-icon__container">
           {props.children}
+        </div>
       </IconContext.Provider>
     </a>
   </li>
