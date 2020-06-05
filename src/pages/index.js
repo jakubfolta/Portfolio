12d67e9c8b579ad1d1f97c89ccdf8a6f025b9a79
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-06-05 10:49:07 +0100

    Create home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,11 @@ const Portfolio = () => {
       <Header>
         <SocialsList />
       </Header>
-      <p>Hello my first portfolio!</p>
+      <h1 className="heading-primary">Hello, <br /> <span>Welcome to my haven!</span></h1>
+      <h2 className="heading-secondary">My name is Jakub Folta.</h2>
+      <h2 className="heading-secondary">I am Front End Developer.</h2>
+      <h2 className="heading-secondary">Take a look at my <span>web portfolio page</span>.</h2>
+
     </Layout>
   )
 }
