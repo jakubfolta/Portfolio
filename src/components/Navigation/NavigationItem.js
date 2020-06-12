e61901943674f40ac322597139e993f9a29949a0
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-06-12 10:05:55 +0100

    Create home page - NavigationList

---
+++ src/components/Navigation/NavigationItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const NavigationItem = props => (
+  <li className="navigation__item">
+    <Link to={props.link} className="navigation__link">{props.children}</Link>
+  </li>
+)
+
+export default NavigationItem;
--- src/components/Navigation/NavigationList.js
+++ src/components/Navigation/NavigationList.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import NavigationItem from './NavigationItem';
+
 const NavigationList = () => (
   <nav className="navigation">
     <ul className="navigation__list">
-
+      <NavigationItem link="/about">About me</NavigationItem>
+      <NavigationItem link="/portfolio">Portfolio</NavigationItem>
+      <NavigationItem link="/contact">Contact</NavigationItem>
     </ul>
   </nav>
 )
