92b961520ee8648b365fcc7dd33bd59012b5e95c
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-06-15 20:55:53 +0100

    Create Navigation list

--- src/components/Navigation/NavigationItem.js
+++ src/components/Navigation/NavigationItem.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-const NavigationItem = props => (
-  <li className="navigation__item">
-    <Link to={props.link} className="navigation__link">{props.children}</Link>
-  </li>
-)
+const NavigationItem = props => {
+
+  return (
+    <li className="navigation__item">
+      <Link to={props.link} className="navigation__link">{props.children}</Link>
+    </li>
+  )
+};
 
 export default NavigationItem;
--- src/components/Navigation/NavigationList.js
+++ src/components/Navigation/NavigationList.js
@@ -2,14 +2,18 @@ import React from 'react';
 
 import NavigationItem from './NavigationItem';
 
-const NavigationList = () => (
-  <nav className="navigation">
-    <ul className="navigation__list">
-      <NavigationItem link="/about">About me</NavigationItem>
-      <NavigationItem link="/portfolio">Portfolio</NavigationItem>
-      <NavigationItem link="/contact">Contact</NavigationItem>
-    </ul>
-  </nav>
-);
+const NavigationList = props => {
+  const navigation = ['navigation']
+
+  return (
+    <nav className={navigation.join(' ')}>
+      <ul className="navigation__list">
+        <NavigationItem link="/about">About me</NavigationItem>
+        <NavigationItem link="/portfolio">Portfolio</NavigationItem>
+        <NavigationItem link="/contact">Contact</NavigationItem>
+      </ul>
+    </nav>
+  )
+};
 
 export default NavigationList;
