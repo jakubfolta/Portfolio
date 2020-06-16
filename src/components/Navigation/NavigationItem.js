3f0988e3fa19c65d22eeffa515147bce12a86131
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-06-16 11:38:15 +0100

    Create Navigation list

--- src/components/Navigation/NavigationItem.js
+++ src/components/Navigation/NavigationItem.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-const NavigationItem = props => {
-  const classes = ['navigation__item'];
-
-  if (props.show) {
-    classes.push('slide');
-  }
-
-  return (
-    <li className={classes.join(' ')}>
-      <Link to={props.link} className="navigation__link">{props.children}</Link>
-    </li>
-  )
-};
+const NavigationItem = props => (
+  <li className="navigation__item">
+    <Link to={props.link} className="navigation__link">{props.children}</Link>
+  </li>
+);
 
 export default NavigationItem;
