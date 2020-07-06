f7ff8a164ed7300d8008aefbd4d9eb351fa8e4bd
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-07-06 20:47:28 +0100

    Create projects page

--- src/components/Projects/ProjectsList.js
+++ src/components/Projects/ProjectsList.js
@@ -19,23 +19,16 @@ const ProjectsList = () => {
     `
   );
 
-  console.log(data);
-
-  const projects = data.site.siteMetadata.projects.map(project =>
+  const projects = data.site.siteMetadata.projects.map( (project, i) =>
     <ProjectItem
+     key={i}
      title={project.title}
      description={project.description} /> )
 
-  // console.log(projects);
-
   return (
-
     <ul className="projects__list">
-      {/* <ProjectItem
-        title="Example"
-        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." /> */}
-        {projects}
-      </ul>
+      {projects}
+    </ul>
   )
 };
 
