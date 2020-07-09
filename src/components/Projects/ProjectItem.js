86770b6b210b7a90748f6b58f15d697f22162d42
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-07-10 00:11:00 +0100

    Create projects page

--- src/components/Projects/ProjectItem.js
+++ src/components/Projects/ProjectItem.js
@@ -6,22 +6,22 @@ import { FaLink } from 'react-icons/fa';
 
 const ProjectItem = props => (
   <li className="projects__item">
-    <h3 className="projects__item-title">{props.title}</h3>
     <div className="projects__image-container">
+      <h3 className="projects__item-title">{props.title}</h3>
       <Img className="projects__image" fluid={props.image} alt="Screenshot of project" />
     </div>
 
     <div className="projects__description-container">
       <p className="projects__description-text">{props.description}</p>
       <p className="projects__description-technologies">{props.technologies.join(' - ')}</p>
+
+      <div className="projects__buttons-container">
+        <a className="projects__button" href={props.githublink} alt="Source code" target="_blank" rel="noopener noreferrer"><FaGithub />Source</a>
+        <a className="projects__button" href={props.demolink} alt="Demo" target="_blank" rel="noopener noreferrer"><FaLink />Demo</a>
+      </div>
     </div>
 
 
-    <div className="projects__buttons-container">
-      <a className="projects__button" href={props.githublink} alt="Source code" target="_blank" rel="noopener noreferrer"><FaGithub />Source</a>
-
-      <a className="projects__button" href={props.demolink} alt="Demo" target="_blank" rel="noopener noreferrer"><FaLink />Demo</a>
-    </div>
   </li>
 );
 
