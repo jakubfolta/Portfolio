a3597034bc2acab49e0631e5977f3a957792bd8d
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-07-16 14:37:55 +0100

    Create contact page

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,33 +3,66 @@ import React from 'react';
 import { FaUser, FaEnvelope, FaEdit } from 'react-icons/fa';
 import { IconContext } from 'react-icons';
 
-const ContactForm = () => (
+const ContactForm = props => (
   <form className="form" method="post" action="/">
     <div className="form__group">
       <IconContext.Provider value={{className: "form__icon"}}>
         <FaUser />
       </IconContext.Provider>
-      <input className="form__input" type="text" name="name" placeholder="Full name" id="name" required autocomplete="off"/>
-      <label className="form__label" for="name">Full name</label>
+      <input
+        className="form__input"
+        type="text"
+        name="name"
+        placeholder="Full name"
+        id="name"
+        required
+        autocomplete="off"
+        value={props.textValue}
+        onChange={props.change} />
+      <label
+        className="form__label"
+        for="name">Full name</label>
     </div>
 
     <div className="form__group">
       <IconContext.Provider value={{className: "form__icon"}}>
         <FaEnvelope />
       </IconContext.Provider>
-      <input className="form__input" type="email" name="email" placeholder="Email" id="email" required autocomplete="off" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"/>
-      <label className="form__label" for="email">Email</label>
+      <input
+        className="form__input"
+        type="email"
+        name="email"
+        placeholder="Email"
+        id="email"
+        required
+        autocomplete="off"
+        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+        value={props.emailValue}
+        onChange={props.change} />
+      <label
+        className="form__label"
+        for="email">Email</label>
     </div>
 
     <div className="form__group">
       <IconContext.Provider value={{className: "form__icon"}}>
         <FaEdit />
       </IconContext.Provider>
-      <textarea className="form__textarea" name="message" id="message" placeholder="Message..."></textarea>
-      <label className="form__label" for="message"></label>
+      <textarea
+        className="form__textarea"
+        name="message"
+        id="message"
+        placeholder="Message..."></textarea>
+      <label
+        className="form__label"
+        for="message"></label>
     </div>
 
-    <input className="form__submit" type="submit" value="Send" />
+    <input
+      className="form__submit"
+      type="submit"
+      value="Send"
+      disabled={props.formIsValid} />
   </form>
 );
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,10 @@ import ContactForm from '../components/ContactForm';
 
 class Contact extends Component {
   state = {
-    menuClicked: false
+    menuClicked: false,
+    name: '',
+    email: '',
+    validForm: false
   }
 
   navVisibilityHandler = () => {
@@ -22,7 +25,11 @@ class Contact extends Component {
         <section className="section section--contact">
           <h2 className="heading-primary page__heading">So, let's talk ;)</h2>
           <p className="section__text">Either you want to ask me about something or just to get my attention, fill up this complicated form below or simply send me an email to <a className="link" href="mailto:jakubfolta@yahoo.co.uk">jakubfolta@yahoo.co.uk</a>.</p>
-          <ContactForm />
+          <ContactForm
+            textValue={this.state.name}
+            emailValue={this.state.email}
+            change={e => this.onChangeHandler(e)}
+            formIsValid={!this.state.validForm} />
         </section>
       </Layout>
     )
