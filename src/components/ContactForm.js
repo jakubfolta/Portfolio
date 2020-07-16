0530a7943235af840ab702664092301dd20ccd5a
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-07-16 02:44:26 +0100

    Create contact page

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import { FaReadme } from 'react-icons/fa';
+import { FaUser, FaEnvelope, FaEdit } from 'react-icons/fa';
 import { IconContext } from 'react-icons';
 
 const ContactForm = () => (
   <form className="form" method="post" action="/">
     <div className="form__group">
       <IconContext.Provider value={{className: "form__icon"}}>
-        <FaReadme />
+        <FaUser />
       </IconContext.Provider>
       <input className="form__input" type="text" name="name" placeholder="Full name" id="name" required autocomplete="off"/>
       <label className="form__label" for="name">Full name</label>
@@ -15,7 +15,7 @@ const ContactForm = () => (
 
     <div className="form__group">
       <IconContext.Provider value={{className: "form__icon"}}>
-        <FaReadme />
+        <FaEnvelope />
       </IconContext.Provider>
       <input className="form__input" type="email" name="email" placeholder="Email" id="email" required autocomplete="off" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"/>
       <label className="form__label" for="email">Email</label>
@@ -23,7 +23,7 @@ const ContactForm = () => (
 
     <div className="form__group">
       <IconContext.Provider value={{className: "form__icon"}}>
-        <FaReadme />
+        <FaEdit />
       </IconContext.Provider>
       <textarea className="form__textarea" name="message" id="message" placeholder="Message..."></textarea>
       <label className="form__label" for="message"></label>
