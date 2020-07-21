c738a7fb92d2a2533fb8b586bf631a9e34cefc7d
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-07-21 12:00:18 +0100

    Create contact page

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 
 import Layout from '../containers/layout';
 import { Link } from 'gatsby';
-import { IconContext } from 'react-icons';
-import { FaAngleDoubleDown } from 'react-icons/fa';
 
 class Thanks extends Component {
   state = {
@@ -24,12 +22,9 @@ class Thanks extends Component {
         clicked={this.navVisibilityHandler} >
         <section className="section section--thanks">
           <h2 className="heading-primary page__heading">Thanks for your message!</h2>
-          <p className="section__text section__text--first">I'll get back to you in a <span className="section__flash">flash</span> <span className="section__wink">;)</span></p> <br />
-          <p className="section__text section__text--second">
-            Now go back to the <Link to="/" className="link">main haven</Link> or check other pages
-            <IconContext.Provider value={{className: "thanks-page__icon"}}>
-              <FaAngleDoubleDown />
-            </IconContext.Provider>
+          <p className="section__text section__text--thanks-1">I'll get back to you in a <span className="section__thanks-flash">flash</span> <span className="section__thanks-wink">;)</span></p> <br />
+          <p className="section__text section__text--thanks-2">
+            Now relax, go back to the <Link to="/" className="link">main haven</Link> or check other pages.
           </p>
         </section>
       </Layout>
