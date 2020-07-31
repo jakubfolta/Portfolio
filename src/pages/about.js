3d300951c68102c640213ae7ff7b11f4965a49b6
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-07-31 14:07:17 +0100

    Remove <br /> from text

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,9 +13,9 @@ class About extends Component {
       <Layout title="Folta | Front End Developer">
         <section className="section">
           <h2 className="heading-primary page__heading">About me</h2>
-          <p className="section__text">I'm <span className="highlight highlight--1">Jacob</span>, passionate self-taught <br/><span className="highlight highlight--2">Web / Front-End Developer</span>.</p><br/>
+          <p className="section__text">I'm <span className="highlight highlight--1">Jacob</span>, passionate self-taught <br/><span className="highlight highlight--2">Web / Front-End Developer</span>.</p>
 
-          <p className="section__text">In my life I've tried many different professions but never really loved what I was doing. Fortunately, in August 2018 I became interested in <br/><span className="highlight highlight--3">code</span>. From that moment on I started my journey with my new <br/><span className="highlight highlight--4">passion</span>.</p><br/>
+          <p className="section__text">In my life I've tried many different professions but never really loved what I was doing. Fortunately, in August 2018 I became interested in <span className="highlight highlight--3">code</span>. From that moment on I started my journey with my new <span className="highlight highlight--4">passion</span>.</p>
 
           <p className="section__text">I've worked through many courses like: <a className="link" href="https://kodilla.com/en/bootcamp/webdeveloper?type=wdp" target="_top">Kodilla (Web Developer Bootcamp),</a>
 
@@ -26,13 +26,13 @@ class About extends Component {
 
           <a className="link" href="https://www.udemy.com/react-the-complete-guide-incl-redux/" target="_top">React - The Complete Guide (incl Hooks, React Router, Redux)</a> and read / watch countless articles and youtube tutorials about Front-end Development. Recently I also obtained <a className="link" href={data.frontmatter.foundations.publicURL} target="_blank" rel="noopener noreferrer">CIW Web Foundations Associate,</a>
 
-          <a className="link" href={data.frontmatter.html.publicURL} target="_blank" rel="noopener noreferrer">CIW Advanced HTML5 and CSS3 Specialist</a> and CIW JavaScript Specialist certificates.</p><br/>
+          <a className="link" href={data.frontmatter.html.publicURL} target="_blank" rel="noopener noreferrer">CIW Advanced HTML5 and CSS3 Specialist</a> and CIW JavaScript Specialist certificates.</p>
 
-          <p className="section__text">{data.frontmatter.extension}</p><br/>
+          <p className="section__text">{data.frontmatter.extension}</p>
 
-          <p className="section__text">To document my journey I push all my code to <a className="link" href="https://github.com/jakubfolta" target="_blank" rel="noopener noreferrer">Github</a> and publish posts on <a className="link" href="https://www.instagram.com/jakub.folta/" target="_blank" rel="noopener noreferrer">Instagram.</a></p><br/>
+          <p className="section__text">To document my journey I push all my code to <a className="link" href="https://github.com/jakubfolta" target="_blank" rel="noopener noreferrer">Github</a> and publish posts on <a className="link" href="https://www.instagram.com/jakub.folta/" target="_blank" rel="noopener noreferrer">Instagram.</a></p>
 
-          <p className="section__text">{data.frontmatter.conclusion}</p><br/>
+          <p className="section__text">{data.frontmatter.conclusion}</p>
 
           <p className="section__text quote-title"><span className="highlight highlight--5">My favorite quote</span></p>
           <blockquote className="quote">{data.frontmatter.quote}</blockquote>
--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -7,7 +7,7 @@ const Thanks = () => (
   <Layout title="Thanks - Folta | Front End Developer">
     <section className="section section--thanks">
       <h2 className="heading-primary page__heading">Thanks for your message!</h2>
-      <p className="section__text section__text--thanks-1">I'll get back to you in a <span className="section__thanks-flash">flash</span> <span className="section__thanks-wink">;)</span></p> <br />
+      <p className="section__text section__text--thanks-1">I'll get back to you in a <span className="section__thanks-flash">flash</span> <span className="section__thanks-wink">;)</span></p>
       <p className="section__text section__text--thanks-2">
         Now relax, go back to the <Link to="/" className="link" title="Start again ;)">main haven</Link> or check other pages.
       </p>
