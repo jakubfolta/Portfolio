27437631b28a314de5ce360c9563be6d1021d0a3
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-07-31 14:15:11 +0100

    Remove <br /> from text

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,7 @@ const HomeIndex = () => (
     <Socials />
     <section className="section section--home">
       <h1 className="heading-primary">Hello, my name is Jakub Folta.</h1>
-      <p className="section__text">I am passionate Front End Developer based in London.</p>
-      <p className="section__text">Take a look at my <Link to="/portfolio" className="link" title="Check my projects">web portfolio page</Link> or send me an <a className="link" href="mailto:jakubfolta@yahoo.co.uk" target="_top">email</a>.</p>
+      <p className="section__text">I am passionate Front End Developer based in London. <br />Take a look at my <Link to="/portfolio" className="link" title="Check my projects">web portfolio page</Link> or send me an <a className="link" href="mailto:jakubfolta@yahoo.co.uk" target="_top">email</a>.</p>
     </section>
   </Layout>
 )
