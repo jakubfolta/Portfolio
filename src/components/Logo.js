f77eecbc1898eaa8bd506e0371cccf8d89a1fa69
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-08-05 13:43:24 +0100

    Add logo, favicon

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
-const Logo = () => (
-  <div className="logo">
-    <p>Logo</p>
-  </div>
-);
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
+
+const Logo = () => {
+  const data = useStaticQuery(
+    graphql`
+      query MyLogo {
+        file(relativePath: {eq: "assets/logo.png"}) {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `
+  );
+
+  return (
+    <div className="logo__container">
+      <Img className="logo__image" fluid={data.file.childImageSharp.fluid} alt="Logo" />
+    </div>
+  )
+};
 
 export default Logo;
